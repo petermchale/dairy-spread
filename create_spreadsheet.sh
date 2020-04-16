7acffb09cb89cd97161e4cc34b7f818d25695f1c
Peter McHale <peter.thomas.mchale@gmail.com>
2020-04-16 15:37:11 -0600

    set image widths

--- create_spreadsheet.sh
+++ create_spreadsheet.sh
@@ -2,7 +2,7 @@ bed="test.bed"
 credentials="credentials.json"
 my_email="peter.thomas.mchale@gmail.com"
 
-python create_spreadsheet.py \
+create_spreadsheet \
   --bed ${bed} \
   --title "test data" \
   --credentials ${credentials} \
