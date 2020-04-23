2f1e124a364a77a679c6f2272441d72b5cd25145
Peter McHale <peter.thomas.mchale@gmail.com>
2020-04-23 16:38:15 -0600

    Delete fetch_igv_urls.sh

--- fetch_igv_urls.sh
+++
@@ -1,3 +0,0 @@
-missed_SVs="/scratch/ucgd/lustre-work/quinlan/u6018199/chaisson_2019/analysis/call_visualizations/manta_0.75_svabaContigs/HG00514.DEL.gt100.lt105.minus_1bp.manta/HG00514.BIP-unified.filtered.DEL.gt100.lt105.minus_1bp.manta.graphite" 
-sort --version-sort -k1,1 -k2,2 "${missed_SVs}.bed" > "${missed_SVs}.sorted.bed" 
-python fetch_igv_urls.py "${missed_SVs}.sorted.bed" > "${missed_SVs}.sorted.sampled.bed" 
